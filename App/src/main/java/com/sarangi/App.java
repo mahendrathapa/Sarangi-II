ca6068dc25cbd21f1c871282087c1c7805cc1bdd
Mahendra Thapa <lazymahendrathapa@gmail.com>
2016-05-29 19:10:40 +0545

    Maven is installed

---
+++ App/src/main/java/com/sarangi/App.java
@@ -0,0 +1,13 @@
+package com.sarangi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
