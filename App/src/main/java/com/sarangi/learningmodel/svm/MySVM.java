076e5cff8930bed9dabcc3e8bdce45662d17170c
mehang <mehang_rai07@yahoo.com>
2016-07-25 13:35:05 +0545

    Only pitch included in svm

--- App/src/main/java/com/sarangi/learningmodel/svm/MySVM.java
+++ App/src/main/java/com/sarangi/learningmodel/svm/MySVM.java
@@ -9,7 +9,8 @@ import java.io.FileReader;
 import java.io.IOException;
 
 import java.util.Scanner;
-import java.util.List; import java.util.ArrayList;
+import java.util.List;
+import java.util.ArrayList;
 
 import java.lang.reflect.Type;
 
